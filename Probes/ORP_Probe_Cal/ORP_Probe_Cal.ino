98730ae177520432e6da5b029d096089e837ef18
0x430x640x650x570x690x7A <44929706+0x430x640x650x570x690x7A@users.noreply.github.com>
2020-10-01 12:38:55 -0500

    Update ORP_Probe_Cal.ino

--- Probes/ORP_Probe_Cal/ORP_Probe_Cal.ino
+++ Probes/ORP_Probe_Cal/ORP_Probe_Cal.ino
@@ -1,9 +1,46 @@
+/*
+   Probe type: Gravity: Analog ORP Sensor Meter For Arduino
+   Probe link: https://www.dfrobot.com/product-1071.html
+   Product Wiki: https://wiki.dfrobot.com/Analog_ORP_Meter_SKU_SEN0165_
+
+*/
+
+/* ***UNTESTED***** */
+
+const byte probePin = A2;
+//may need to make long if we have large sample size
+//assumption is max reading is 414 and int only has ~32k so 77 * 414 > 32k
+int rawReading = 0;
+//need float for floating point arithmetic
+float correctedReading = 0;
+//in case we cant find correct gain for calibration
+float myOffset = 0;
+
+int sampleSize = 20;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(probePin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2);
+  //take reading and add it to buffer
+  for (int i = 0; i < sampleSize; ++i) {
+    rawReading += analogRead(probePin);
+    Serial.print("Raw values: ");
+    Serial.println(rawReading, 2);
+    //sampling interval
+    delay(2);
+  }
+  //may need to cast rawreading to float
+  //not sure here 30 and 75 come from maybe scaling value or something to do with sample size
+  //could be something with the hardware
+  //((30 * voltage * 1k?) - 75? * avg reading * voltage * 1k? / 10 bit )) / 75?
+  correctedReading = ((30 * 5.0 * 1000) - (75 * ((float)rawReading / sampleSize) * 5.0 * 1000 / 1024)) / 75; //- OFFSET;
 
+  Serial.print("ORP: ");
+  Serial.println(correctedReading, 2);
+  delay(500);
 }
