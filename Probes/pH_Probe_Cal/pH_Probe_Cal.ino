9d8ac4f85fe69ce7b1cd420ab90ad4babe163551
0x430x640x650x570x690x7A <44929706+0x430x640x650x570x690x7A@users.noreply.github.com>
2020-10-02 09:26:50 -0500

    Update pH_Probe_Cal.ino

--- Probes/pH_Probe_Cal/pH_Probe_Cal.ino
+++ Probes/pH_Probe_Cal/pH_Probe_Cal.ino
@@ -8,13 +8,13 @@
 const byte probePin = A2;
 //may need to make long if we have large sample size
 //assumption is max reading is 414 and int only has ~32k so 77 * 414 > 32k
-int rawReading = 0;
+long rawReading = 0;
 //need float for floating point arithmetic
 float correctedReading = 0;
 //in case we cant find correct gain for calibration
-float myOffset = 0;
+float myOffset = 0.00;
 
-int sampleSize = 20;
+int sampleSize = 200;
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,13 +30,19 @@ void loop() {
     delay(2);
   }
   //may need to cast rawreading to float
-  //scaling factor* (raw * voltage / adc bit size / samples)
-  correctedReading = 3.5 * (rawReading * 5.0 / 1024 / sampleSize); 
+  //scaling factor * ((raw * voltage / adc bit size / samples) + deviation) + correctionValue
+  correctedReading = 3.5 * ((rawReading * 5.0 / 1024 / sampleSize) + 0.05) + myOffset; 
+  //same as line before but broken up
+  //correctedReading = (rawReading * 5.0 / 1024 / sampleSize) + 0.05;
+  //correctedReading = 3.5 * correctedReading + myOffset;
   Serial.print("pH: ");
   Serial.println(correctedReading, 2);
   delay(500);
+  rawReading = 0;
 }
 
+/*
 float myMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+*/
