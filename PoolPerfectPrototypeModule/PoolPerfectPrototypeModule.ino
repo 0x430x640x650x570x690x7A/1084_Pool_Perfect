5340c9063657765e7edff15996baf411c129fc0c
0x430x640x650x570x690x7A <44929706+0x430x640x650x570x690x7A@users.noreply.github.com>
2020-10-26 16:05:33 -0500

    Update PoolPerfectPrototypeModule.ino

--- PoolPerfectPrototypeModule/PoolPerfectPrototypeModule.ino
+++ PoolPerfectPrototypeModule/PoolPerfectPrototypeModule.ino
@@ -9,14 +9,17 @@
 
       to do:
       calculate chlorine
-      pump vvvvvvDelay
+      pump vvDelay
 */
 //include all necessary libraries
 #include <Adafruit_SSD1306.h> // needed for I2C display
 #include "VirtuinoCM.h" // integration with app
-#include "DallasTemperature.h" // for temperature sensor
 
 //include all pin connections and variables
+
+//timer interrupts
+const float interruptInterval = .5; //seconds
+
 //Display init variables
 //Display: I2C
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -24,47 +27,44 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // Display: LEDS
-const byte mainPumpStatusPin = 23;
-const byte chemicalLevelStatusPin = 25;
-const byte sensorArrayStatusPin = 27;
-const byte pumpArrayStatusPin = 29;
+const byte mainPumpStatusPin = 22;
+const byte chemicalLevelStatusPin = 23;
+const byte sensorArrayStatusPin = 24;
+const byte pumpArrayStatusPin = 25;
+
+const byte dispensingChlorine = 26;
+const byte dispensingSodaAsh = 27;
+const byte dispensingMuriaticAcid = 28;
 
 // Display: App
 VirtuinoCM virtuino;
-boolean debug = false;              // set this variable to false on the finale code to decrease the request time.
+boolean debug = true;              // set this variable to false on the finale code to decrease the request time.
 
 //level sensor init variables
-const byte levelPinChlorine = 13;
-const byte levelPinMuriatic = 14;
-const byte levelPinSodaAsh = 15;
+const byte levelPinChlorine = 10;
+const byte levelPinMuriatic = 11;
+const byte levelPinSodaAsh = 9;
 
 //probe init variables
-const byte probePinPH = A0;
-const byte probePinORP = A1;
-const byte probePinTemp = 6;
 const byte samplingInterval = 10; //ms
 
-// Probe: Temp
-//set data from sensor
-OneWire dataWireTempProbe(probePinTemp);
-//create sensor object to use function calls
-DallasTemperature tempSensor(&dataWireTempProbe);
-
 //relay init variables
-const byte relayPin0Chlorine = 31;
-const byte relayPin1Muriatic = 33;
-const byte relayPin2SodaAsh = 35;
-const byte standbyTime = .2; // in min
+const byte relayPin0Chlorine = 35;
+const byte relayPin2Muriatic = 39;
+const byte relayPin1SodaAsh = 37;
+const byte standbyTime = 1; // in min
 int pumpDelay = 5; //seconds
 
 //current clamp init variables
-const byte currentClampPin = A0;         //set arduino signal read pin
+const byte currentClampPin = A3;         //set arduino signal read pin
 const byte ACTectionRange = 10; //set Non-invasive AC Current Sensor tection range (5A,10A,20A)
 const byte currentCutOff = 5; // amps
 
 byte sampleSize = 20;
 
 //function prototypes
+void initStatusPins();
+void initTimer();
 void initApp();
 void initDisplay();
 void initLevelSensor();
@@ -88,56 +88,105 @@ struct sensorInfo {
   //need float for floating point arithmetic
   float V0_lastValuePH = 7.5;
   //in case we cant find correct gain for calibration
-  float V1_lastValueChlorine = 2.50;
+  float V1_lastValueChlorine = 2.5;
   int V2_lastValueTemp = 88;
   //v3 is ph indicator
   //v4 is chlorine indicator
   static const byte V_memory_count = 5;          // the size of V memory can change it to a number <=255)
-  float V[V_memory_count] = {V0_lastValuePH, V1_lastValueChlorine, V2_lastValueTemp};           // This array is synchronized with Virtuino V memory. You can change the type to int, long etc.
+  //want to update sliders and displays at setup
+  float V[V_memory_count] = {V0_lastValuePH, V1_lastValueChlorine, V2_lastValueTemp, V0_lastValuePH, V1_lastValueChlorine};           // This array is synchronized with Virtuino V memory. You can change the type to int, long etc.
 } sensor;
 //sensorInfo sensor;
 
 void setup() {
+  //Serial.begin(9600); // interact with serial monitor
+  initStatusPins();
   virtuinoRun();
+  initTimer();
   initApp();
   initDisplay();
   initLevelSensor();
   initProbe();
   initRelay();
   pinMode(currentClampPin, INPUT); // init current clamp input
-  digitalWrite(mainPumpStatusPin, LOW);
-  digitalWrite(chemicalLevelStatusPin, LOW);
-  digitalWrite(sensorArrayStatusPin, LOW);
-  digitalWrite(pumpArrayStatusPin, LOW);
 
 }
 
+/*
+  ISR(TIMER4_COMPA_vect) {
+  virtuinoRun();
+  updateDisplay();
+  }
+*/
 void loop() {
   vDelay(2);
   //if my main motor is on/true run the while loop
   while (checkMainPump()) {
     //main pump is determined to be on
     //check if chemicals are adequate
+    vDelay(500);
     if (checkChemicalLevels()) {
       //if i have enough chemicals then i want to check probes and update display
+      vDelay(500);
       checkProbes();
-      updateDisplay();
+      //updateDisplay();
       //run my pumps
+      vDelay(500);
       runPumps();
     } else {
       //write to LED to refill chemicals in checking chemicals function
     }
     //wait after ive dispensed some chemicals
     //look more into sleep function from arduino library
-    doNothing();
-    //vDelay(1000);
+    //doNothing();
+    vDelay(5000);
   }
   //dont want to continuously check main pump power
-  doNothing();
-  
+  //doNothing();
+
   //need to wakeup
-  //vDelay(1000);
+  vDelay(5000);
 }
+
+void initStatusPins() {
+  pinMode(mainPumpStatusPin, INPUT);
+  pinMode(chemicalLevelStatusPin, INPUT);
+  pinMode(sensorArrayStatusPin, INPUT);
+  pinMode(pumpArrayStatusPin, INPUT);
+
+  pinMode(dispensingChlorine, INPUT);
+  pinMode(dispensingSodaAsh, INPUT);
+  pinMode(dispensingMuriaticAcid, INPUT);
+
+  digitalWrite(mainPumpStatusPin, LOW);
+  digitalWrite(chemicalLevelStatusPin, LOW);
+  digitalWrite(sensorArrayStatusPin, LOW);
+  digitalWrite(pumpArrayStatusPin, LOW);
+
+  digitalWrite(dispensingChlorine, LOW);
+  digitalWrite(dispensingSodaAsh, LOW);
+  digitalWrite(dispensingMuriaticAcid, LOW);
+}
+
+void initTimer() {
+  /*
+    cli();//stop interrupts
+    //set timer4 interrupt at 1Hz
+    TCCR4A = 0;// set entire TCCR1A register to 0
+    TCCR4B = 0;// same for TCCR1B
+    TCNT4  = 0;//initialize counter value to 0
+    // set compare match register for 1hz increments
+    OCR4A = 15623 * interruptInterval;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+    // turn on CTC mode
+    TCCR4B |= (1 << WGM12);
+    // Set CS12 and CS10 bits for 1024 prescaler
+    TCCR4B |= (1 << CS12) | (1 << CS10);
+    // enable timer compare interrupt
+    TIMSK4 |= (1 << OCIE4A);
+    sei();//allow interrupts
+  */
+}
+
 
 void initApp() {
   if (debug) {
@@ -154,7 +203,7 @@ void initDisplay() {
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
   }
-  vDelay(2000);
+  //vDelay(1000);
   display.clearDisplay();
 
   display.setTextSize(3);
@@ -163,15 +212,15 @@ void initDisplay() {
   // Display static text
   display.println("BootingSystem");
   display.display();
-  vDelay(2000);
+  vDelay(1000);
   display.setTextSize(2); // could put in init but error messages may not be the correct size
   display.clearDisplay();
   display.setCursor(0, 10);
   // Display static text
   display.print("pH:   ");
-  display.println(sensor.V0_lastValuePH);
+  display.println(sensor.V0_lastValuePH, 1);
   display.print("Cl:   ");
-  display.println(sensor.V1_lastValueChlorine);
+  display.println(sensor.V1_lastValueChlorine, 1);
   display.print("Temp: ");
   display.print(sensor.V2_lastValueTemp);
   display.setTextSize(1);
@@ -186,29 +235,33 @@ void initLevelSensor() {
 }
 
 void initProbe() {
-  pinMode(probePinPH, INPUT);
-  //ORP
-  pinMode(probePinORP, INPUT);
-  //Temp
-  //init sensor
-  tempSensor.begin();
-  //change sensor accuracy
-  tempSensor.setResolution(11);
+  /*
+    pinMode(probePinPH, INPUT);
+    //ORP
+    pinMode(probePinORP, INPUT);
+    //Temp
+    //init sensor
+    tempSensor.begin();
+    //change sensor accuracy
+    tempSensor.setResolution(11);
+  */
 }
 
 void initRelay() {
   // set control line as outputs
   //DDRC |= 0x55; // B0101 0101
   pinMode(relayPin0Chlorine, OUTPUT);
-  pinMode(relayPin1Muriatic, OUTPUT);
-  pinMode(relayPin2SodaAsh, OUTPUT);
+  pinMode(relayPin2Muriatic, OUTPUT);
+  pinMode(relayPin1SodaAsh, OUTPUT);
 
   //relay is active low, set all pins high
   //PORTC &= ~0x55;
   digitalWrite(relayPin0Chlorine, HIGH);
-  digitalWrite(relayPin1Muriatic, HIGH);
-  digitalWrite(relayPin2SodaAsh, HIGH);
+  digitalWrite(relayPin2Muriatic, HIGH);
+  digitalWrite(relayPin1SodaAsh, HIGH);
 }
+
+
 
 //tested
 bool checkMainPump() {
@@ -236,14 +289,14 @@ bool checkMainPump() {
 //tested
 bool checkChemicalLevels() {
   bool readChlorineLevel = 0; // == F
-  bool readMuriaticLevel = 1;
-  bool readSodaAshLevel = 1;
+  bool readMuriaticLevel = 0;
+  bool readSodaAshLevel = 0;
 
   //read level of each level sensor
   //could do if between each read to not waste time reading all values if one is too low
   readChlorineLevel = digitalRead(levelPinChlorine);
-  //readMuriaticLevel = digitalRead(levelPinMuriatic);
-  //readSodaAshLevel = digitalRead(levelPinSodaAsh);
+  readMuriaticLevel = digitalRead(levelPinMuriatic);
+  readSodaAshLevel = digitalRead(levelPinSodaAsh);
 
   //if any reading is low then i need to say it is low
   if (readChlorineLevel && readMuriaticLevel && readSodaAshLevel) {
@@ -261,14 +314,14 @@ void checkProbes() {
   bool newPHReading = false;
   bool newChlorineReading = false;
   bool newTempReading = false;
-
+  vDelay(10);
   digitalWrite(sensorArrayStatusPin, HIGH);
   Serial.println("Sensor Array ON");
-
-  Serial.println("Reading values");
+  //vDelay(1000);
+  //Serial.println("Reading values");
   while (true) {
     if (sensor.V[0] != sensor.V0_lastValuePH) {           // The V0 has changed
-      vDelay(500); // need a delay because sometimes multiple readings sent
+      vDelay(500); // need a vDelay because sometimes multiple readings sent
       //Serial.println("pH = " + String(sensor.V[0]));  // print the value of V0
       sensor.V0_lastValuePH = sensor.V[0];// store the V0 to the variable V0_lastValue so as to know if it has changed
       newPHReading = true;
@@ -278,12 +331,11 @@ void checkProbes() {
     if (sensor.V[1] != sensor.V1_lastValueChlorine) {            // The V1 has changed
       vDelay(500);
       //Serial.println("Chlorine = " + String(sensor.V[1]));  // print the value of V1
-      sensor.V[4] = sensor.V[1]; // update chlorine indicator
       sensor.V1_lastValueChlorine = sensor.V[1];                 // store the V1 to the variable V1_lastValue so as to know if it has changed
       newChlorineReading = true;
     }
     sensor.V[4] = sensor.V[1]; // update chlorine indicator
-    
+
     if (sensor.V[2] != sensor.V2_lastValueTemp) {     // The V1 has changed
       vDelay(500);
       //Serial.println("Temperature = " + String(sensor.V[2]));  // print the value of V1
@@ -298,14 +350,14 @@ void checkProbes() {
   }
   //Serial.println("Sending values");
 
-  vDelay(2000);     // This is an example of the recommended delay function. Remove this if you don't need
+  //vDelay(1000);     // This is an example of the recommended vDelay function. Remove this if you don't need
   Serial.println("Sensor Array OFF");
   digitalWrite(sensorArrayStatusPin, LOW);
 }
 
 //tested
 void updateDisplay() {
-  Serial.println("Updating Display");
+  //Serial.println("Updating Display");
   display.setTextSize(2);
   display.setCursor(0, 10);
   display.clearDisplay();
@@ -322,32 +374,50 @@ void updateDisplay() {
 }
 
 void runPumps() {
+  vDelay(1000);
   digitalWrite(pumpArrayStatusPin, HIGH);
   Serial.println("Pump Array ON");
-
   //check pH thn chlorine
   if (sensor.V0_lastValuePH < sensor.lowBoundPH) {
     //add soda ash
     //calculate vDelay for soda ash
-    digitalWrite(relayPin2SodaAsh, LOW);
+    Serial.println("Pumping Soda Ash");
+    digitalWrite(dispensingSodaAsh, HIGH);
+    digitalWrite(relayPin1SodaAsh, LOW);
     //wait for desired time
     vDelay(pumpDelay * 1000);
-    digitalWrite(relayPin2SodaAsh, HIGH);
+    digitalWrite(relayPin1SodaAsh, HIGH);
+    digitalWrite(dispensingSodaAsh, LOW);
+    sensor.V[0] = 7.5;           // increase ph to 7.5
+    sensor.V0_lastValuePH = sensor.V[0];//update last value for LED and next loop
+    sensor.V[3] = sensor.V[0]; // update ph indicator
   } else if (sensor.V0_lastValuePH > sensor.highBoundPH) {
     //add muraitic acid
     //calculate pump vDelay for muriatic acid
-    digitalWrite(relayPin1Muriatic, LOW);
+    Serial.println("Pumping Muriatic Acid");
+    digitalWrite(dispensingMuriaticAcid, HIGH);
+    digitalWrite(relayPin2Muriatic, LOW);
     //wait for desired time
     vDelay(pumpDelay * 1000);
-    digitalWrite(relayPin1Muriatic, HIGH);
+    digitalWrite(relayPin2Muriatic, HIGH);
+    digitalWrite(dispensingMuriaticAcid, LOW);
+    sensor.V[0] = 7.5;           // decrease ph to 7.5
+    sensor.V0_lastValuePH = sensor.V[0];//update last value for LED and next loop
+    sensor.V[3] = sensor.V[0]; // update ph indicator
   } else if (sensor.V1_lastValueChlorine < sensor.lowBoundChlorine) {
     //increase chlorine ppm
-    //calculate chlorine pumpvDelay
+    //calculate chlorine pumpDelay
     //turn on correct relay
+    Serial.println("Pumping Chlorine");
+    digitalWrite(dispensingChlorine, HIGH);
     digitalWrite(relayPin0Chlorine, LOW);
     //wait for desired time
     vDelay(pumpDelay * 1000);
     digitalWrite(relayPin0Chlorine, HIGH);
+    digitalWrite(dispensingChlorine, LOW);
+    sensor.V[1] = 3;          // update chlorine value
+    sensor.V1_lastValueChlorine = sensor.V[1];  //update last value for LED and next loop
+    sensor.V[4] = sensor.V[1]; // update chlorine indicator
   } else {
     //we cant do anything if chlorine is too high
   }
@@ -358,6 +428,7 @@ void runPumps() {
 
 //untested
 void doNothing() {
+  cli();//stop interrupts
   //loop for one minute
   for (int i = 0; i <= standbyTime; ++i) {
     //wait one minute
@@ -365,12 +436,14 @@ void doNothing() {
     vDelay(30 * 1000);
     vDelay(30 * 1000);
   }
+  sei();//allow interrupts
 }
 
 /* This function is called every time Virtuino app sends a request to server to change a Pin value
    The 'variableType' can be a character like V, T, O  V=Virtual pin  T=Text Pin    O=PWM Pin
    The 'variableIndex' is the pin number index of Virtuino app
    The 'valueAsText' is the value that has sent from the app   */
+
 void onReceived(char variableType, uint8_t variableIndex, String valueAsText) {
   if (variableType == 'V') {
     float value = valueAsText.toFloat();        // convert the value to float. The valueAsText have to be numerical
@@ -403,8 +476,11 @@ void virtuinoRun() {
   }
 }
 
-//virtuino Delay
+//virtuino vDelay
 void vDelay(int vDelayInMillis) {
   long t = millis() + vDelayInMillis;
-  while (millis() < t) virtuinoRun();
+  while (millis() < t) {
+    virtuinoRun();
+    updateDisplay();
+  }
 }
